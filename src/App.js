db2fcbcfce8d46bbd6d24cec06334d929efeee4e
Aziz Kanoun <azizkanoun06@gmail.com>
2023-11-25 23:47:14 +0100

    add bg colour.

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           <Route path='/men' element={<ShopCategory category="men"/>}/>
           <Route path='/women' element={<ShopCategory category="women"/>}/>
           <Route path='/kids' element={<ShopCategory category="kids"/>}/>
-          <Route path='product' element={<Products/>}>
+          <Route path='/product' element={<Products/>}>
             <Route path=':productId' element={<Products/>}/>
           </Route>
           <Route path='/cart' element={<Cart/>}/>
